41bf1dca13685f13811d5263da1f179d4454a350
SilentRaccoon02 <02smrnv@gmail.com>
2023-11-14 20:28:13 +0300

    Configure project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
+#include <emscripten.h>
 #include <iostream>
 
-using namespace std;
+extern "C" {
+EMSCRIPTEN_KEEPALIVE
+float add(float x, float y) { return x + y; }
 
-int main()
-{
-    cout << "Hello World!" << endl;
+// extern void f(int a);
+// extern int g(int a, int b);
+}
+
+typedef void js();
+
+void callJs(int p) { ((js *)p)(); }
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Incorrect main call" << std::endl;
+        return 0;
+    }
+
+    int p = std::stoi(std::string(argv[1]));
+    callJs(p);
+
+    // f(7);
+    // std::cout << "Function g returned " << g(2, 5) << std::endl;
     return 0;
 }
