0f9940340c972de7e0311624d60ec5d1496666a8
SilentRaccoon02 <02smrnv@gmail.com>
2024-02-05 17:03:40 +0300

    Remove tests

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
-#include <cstdint>
-#include <cstdlib>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
 extern "C" {
-uint8_t *processImage(uint8_t *image, uint16_t width, uint16_t height);
+uint8_t *testArray(uint8_t *inData, uint16_t length);
+uint8_t *processImage(uint8_t *inPointer, uint16_t width, uint16_t height);
 }
 
-uint8_t *processImage(uint8_t *image, uint16_t width, uint16_t height) {
-    Mat inMat(height, width, CV_8UC4);
-    memcpy(inMat.data, image, width * height * 4 * sizeof(uint8_t));
+uint8_t *testArray(uint8_t *inPointer, uint16_t length) {
+    uint8_t *outPointer = static_cast<uint8_t *>(malloc(length * sizeof(uint8_t)));
+
+    for (uint16_t i = 0; i < length; ++i) {
+        outPointer[i] = inPointer[i] * 2;
+    }
+
+    return outPointer;
+}
+
+uint8_t *processImage(uint8_t *inPointer, uint16_t width, uint16_t height) {
+    Mat inImage(height, width, CV_8UC4);
+    size_t inSize = height * width * 4 * sizeof(uint8_t);
+    memcpy(inImage.data, inPointer, inSize);
 
     Mat canny;
-    Mat outMat;
-    Canny(inMat, canny, 80, 240);
-    cvtColor(canny, outMat, COLOR_GRAY2RGBA);
+    Mat outImage;
+    Canny(inImage, canny, 80, 240);
+    cvtColor(canny, outImage, COLOR_GRAY2RGBA);
 
-    size_t outImageSize = outMat.total() * outMat.elemSize();
-    uint8_t *outImage = static_cast<uint8_t *>(malloc(outImageSize));
-    memcpy(outImage, outMat.data, outImageSize);
-    return outImage;
+    size_t outSize = outImage.total() * outImage.elemSize();
+    uint8_t *outPointer = static_cast<uint8_t *>(malloc(outSize));
+    memcpy(outPointer, outImage.data, outSize);
+    return outPointer;
 }
--- tests.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-extern "C" {
-float testAdd(float a, float b);
-void testMultiply(char *str_p);
-void testArray(uint8_t *in, uint8_t size, uint8_t delta);
-}
