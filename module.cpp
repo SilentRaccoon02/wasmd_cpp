e8ae3db62d501be229257995d55df9484c1e01ee
SilentRaccoon02 <02smrnv@gmail.com>
2024-03-01 17:02:19 +0300

    Configure project

--- module.cpp
+++ module.cpp
@@ -4,21 +4,10 @@ using namespace cv;
 using namespace std;
 
 extern "C" {
-uint8_t *testArray(uint8_t *inData, uint16_t length);
-uint8_t *processImage(uint8_t *inPointer, uint16_t width, uint16_t height);
+uint8_t *run(uint8_t *inPointer, uint16_t width, uint16_t height);
 }
 
-uint8_t *testArray(uint8_t *inPointer, uint16_t length) {
-    uint8_t *outPointer = static_cast<uint8_t *>(malloc(length * sizeof(uint8_t)));
-
-    for (uint16_t i = 0; i < length; ++i) {
-        outPointer[i] = inPointer[i] * 2;
-    }
-
-    return outPointer;
-}
-
-uint8_t *processImage(uint8_t *inPointer, uint16_t width, uint16_t height) {
+uint8_t *run(uint8_t *inPointer, uint16_t width, uint16_t height) {
     Mat inImage(height, width, CV_8UC4);
     size_t inSize = height * width * 4 * sizeof(uint8_t);
     memcpy(inImage.data, inPointer, inSize);
