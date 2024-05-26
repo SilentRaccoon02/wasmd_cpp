3b976f6b56f1d8d6ec4a3256726e9284c543db43
SilentRaccoon02 <02smrnv@gmail.com>
2024-05-26 15:59:22 +0300

    Configure project

--- module.cpp
+++ module.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -35,5 +36,7 @@ uint8_t *run(uint8_t *inPointer, uint16_t width, uint16_t height) {
     size_t outSize = outImage.total() * outImage.elemSize();
     uint8_t *outPointer = static_cast<uint8_t *>(malloc(outSize));
     memcpy(outPointer, outImage.data, outSize);
+
+    cout << "log" << endl;
     return outPointer;
 }
---
+++ pre.js
@@ -0,0 +1,3 @@
+var Module = {
+    'print': function (text) { postMessage({ text }) }
+}
